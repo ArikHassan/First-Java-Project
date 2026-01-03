6ca170c13cb48719b90c45019f3b77c38a853121
ArikHassan <arik-h@hotmail.com>
2026-01-02 20:53:27 -0500

    Initial Commit. App prints message.

---
+++ README.md
@@ -0,0 +1,5 @@
+# FirstJavaProject
+---
+This is my first time learning Java, and I'm super excited to document my
+journey. In this simple program I'll demonstrate some basic knowledge of Java
+programming.
---
+++ src/main/java/com/BasicApp/BasicApp.java
@@ -0,0 +1,13 @@
+package com.BasicApp;
+/* The package statement is the first actual statement in a Java file (after comments)
+and groups related classes and interfaces together. For example: package com.example.util;
+
+BasicApp.java needs to be inside directory called "com.BasicApp", else there
+will be errors in the package statement on line 1.
+*/
+
+public class BasicApp {
+    public static void main(String[] args) {
+        System.out.println("Basic App - This is a beginner level Java Application.");
+    }
+}
