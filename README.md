64417f62df57207d5fa1f63c3bff27b1e18860df
ArikHassan <arik-h@hotmail.com>
2026-01-02 21:46:28 -0500

    Updated README, Read user input (BasicApp.java)

--- README.md
+++ README.md
@@ -1,5 +1,10 @@
 # FirstJavaProject
----
-This is my first time learning Java, and I'm super excited to document my
-journey. In this simple program I'll demonstrate some basic knowledge of Java
-programming.
+
+## Project Description
+Hi there, my name is Arik Hassan and this is my first project in Java.
+I'm super excited to document my journey to becoming an expert in using Java to build professional apps. In this simple program I'll demonstrate some basic knowledge of the Java
+language.
+
+### Concepts covered in this application:
+- Printing strings
+- Reading user input from console and printing it
--- src/main/java/com/BasicApp/BasicApp.java
+++ src/main/java/com/BasicApp/BasicApp.java
@@ -6,8 +6,27 @@ BasicApp.java needs to be inside directory called "com.BasicApp", else there
 will be errors in the package statement on line 1.
 */
 
+// Scanner --> Scanner object built into Java to read user input
+import java.util.Scanner;
+
 public class BasicApp {
     public static void main(String[] args) {
+
+        // Header message
         System.out.println("Basic App - This is a beginner level Java Application.");
+        System.out.println("------------------------------------------------------");
+
+        // Create tool to read user input and name it scanner
+        Scanner scanner = new Scanner(System.in);
+
+        // Prompt user for input (name)
+        System.out.println("Please enter your Username / Full Name:");
+
+        // Create a variable type: String to store user input (name)
+        String userName = scanner.nextLine(); // reads and assigns the next user input
+
+        // Print a message using the users input
+        System.out.println("\nGreetings " + userName + " and welcome to this Java application.\n" +
+                "Please read the documentation in the projects' README.md file to get details about this program.");
     }
 }
